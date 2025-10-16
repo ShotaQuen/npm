9920d55a372e78d8d5b5c1c8b3a9032ae711c745
Ikann <101172489+ShotaQuen@users.noreply.github.com>
2025-10-16 14:41:52 +0800

    Create download-spotify.js

---
+++ scrape/download-spotify.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import * as cheerio from 'cheerio'
+
+const downloadSpotify = async (query) => {
+  try {
+    if (!url.includes('open.spotify.com')) throw new Error('Invalid url.');
+
+    const rynn = await axios.get('https://spotdl.io/', {
+        headers: {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+    });
+    const $ = cheerio.load(rynn.data);
+
+    const api = axios.create({
+        baseURL: 'https://spotdl.io',
+        headers: {
+            cookie: rynn.headers['set-cookie'].join('; '),
+            'content-type': 'application/json',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'x-csrf-token': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+
+    const [{ data: meta }, { data: dl }] = await Promise.all([
+        api.post('/getTrackData', { spotify_url: url }),
+        api.post('/convert', { urls: url })
+    ]);
+
+    return {
+        ...meta,
+        download_url: dl.url
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export default downloadSpotify
